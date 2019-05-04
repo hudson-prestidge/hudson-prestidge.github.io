61e766c74db47cfb03fdb24a157192dda62d549e
Hudson Burns Prestidge <hudson.prestidge@gmail.com>
2019-05-05 05:02:12 +1200

    reconfigure webpack for new github pages layout

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,5 @@
+const path = require('path')
+
 module.exports = {
   module: {
     rules: [
@@ -9,5 +11,8 @@ module.exports = {
         }
       }
     ]
+  },
+  output: {
+    path: __dirname
   }
 };
